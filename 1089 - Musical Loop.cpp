b3e7e83919cd495dd1fc032a12cef522721969cd
MD APU HOSEN <47341461+APU160139@users.noreply.github.com>
2020-10-02 15:47:21 +0600

    Add files via upload

---
+++ 1089 - Musical Loop.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int a[10000],n,i,x;
+ scanf("%d", &n);
+ while(n != 0)
+ {
+  x = 0;
+  for(i = 0; i < n; i++)
+  {
+  scanf("%d", &a[i]);
+  }
+
+  for(i = 0; i < n; i++)
+  {
+   if(a[i] < a[(i + 1)%n])
+   {
+    if(a[(i + 1)%n] > a[(i + 2)%n])
+    {
+     x++;
+    }
+
+   }
+
+   if(a[i] > a[(i + 1)%n])
+   {
+    if(a[(i + 1)%n] < a[(i + 2)%n])
+    {
+     x++;
+    }
+
+   }
+  }
+  printf("%d\n", x);
+  scanf("%d", &n);
+
+ }
+
+ return 0;
+
+}
