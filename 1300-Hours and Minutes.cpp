a4a7132465a75aca7036af640221ff71fe2d226e
MD APU HOSEN <47341461+APU160139@users.noreply.github.com>
2021-01-19 20:47:57 +0600

    Add files via upload

---
+++ 1300-Hours and Minutes.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int kon;
+    while(scanf("%d",&kon)!= EOF)
+    {
+        if(kon%6==0){
+          printf("Y\n");
+        }
+        else{
+          printf("N\n");
+        }
+    }
+    return 0;
+}
