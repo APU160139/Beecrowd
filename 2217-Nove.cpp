cae90b19bd4def757e5d43f7f957cc2a9ecabcf3
MD APU HOSEN <47341461+APU160139@users.noreply.github.com>
2021-06-12 19:48:07 +0600

    Add files via upload

---
+++ 2217-Nove.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+	int n,t;
+	scanf("%d",&t);
+	while(t--)
+	{
+		scanf("%d",&n);
+		if(n%2==0){
+			printf("1\n");
+		}
+		else{
+			printf("9\n");
+		}
+	}
+   return 0;
+}
